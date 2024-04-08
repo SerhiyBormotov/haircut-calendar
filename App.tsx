8140bf65c6f8aa6ccc8805d8a445ce00e8c6d14a
Serhii Bormotov <bormotov.lavie@gmail.com>
2024-04-08 12:39:45 +0300

    Create icons and splash image

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "react-native-gesture-handler";
 import { IRootStore, Provider, setupStore } from "./src/store/root.store";
 import RootNavigator from "./src/navigation/RootNavigator";
 import { useFonts } from "expo-font";
-import LoadingScreen from "./src/components/Loading";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
@@ -19,13 +20,21 @@ export default function App() {
     });
   }, []);
 
+  const onAppReady = useCallback(async () => {
+    if (fontsLoaded && store) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, store]);
+
   if ((!fontsLoaded && !fontError) || !store) {
-    return <LoadingScreen />;
+    return null;
   }
 
   return (
-    <Provider value={store}>
-      <RootNavigator />
-    </Provider>
+    <View onLayout={onAppReady} style={{ flex: 1 }}>
+      <Provider value={store}>
+        <RootNavigator />
+      </Provider>
+    </View>
   );
 }
